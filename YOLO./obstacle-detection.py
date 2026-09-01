6deb23bbb459a80d77c66485800b774a907e629d
bansi49 <bansibhimani588@gmail.com>
2026-09-01 15:55:12 +0530

    Update obstacle-detection.py

--- YOLO./obstacle-detection.py
+++ YOLO./obstacle-detection.py
@@ -1,1 +1,57 @@
+from ultralytics import YOLO
+import cv2
 
+model = YOLO("yolo11n.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    results = model(
+        frame,
+        conf=0.40,
+        classes=[1, 2, 3, 5, 7, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
+        verbose=False
+    )
+
+    for result in results:
+        for box in result.boxes:
+
+            cls_id = int(box.cls[0])
+            conf = float(box.conf[0])
+            name = model.names[cls_id]
+
+            if name.lower() == "person":
+                continue
+
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+            cv2.rectangle(
+                frame,
+                (x1, y1),
+                (x2, y2),
+                (0, 255, 0),
+                2
+            )
+
+            cv2.putText(
+                frame,
+                f"{name} {conf:.2f}",
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 255, 0),
+                2
+            )
+
+    cv2.imshow("Object Recognition", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
